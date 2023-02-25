6462f15e0448f5118a22c9bb9188dfdfe6eabd5c
shilpa123 <shilpabiradar100@gmail.com>
2023-02-25 22:21:46 +0530

    Added:Validator For Email

--- src/main/java/org/example/UserMain.java
+++ src/main/java/org/example/UserMain.java
@@ -4,14 +4,36 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserMain {
-    public static boolean firstNameValidation(String FirstName) {
+    public static boolean firstNameValidation(String FirstName){
         String regex = "^[A-Z]{1}[a-z A-Z]{2,}$";
         Pattern p = Pattern.compile(regex);
-        if (FirstName == null) {
+        if (FirstName == null){
             return false;
         }
         Matcher m = p.matcher(FirstName);
         return m.matches();
+
+    }
+
+    public static boolean lastNameValidation(String LastName){
+        String regex = "^[A-Z]{1}[a-z A-Z]{2,}$";
+        Pattern p = Pattern.compile(regex);
+        if (LastName == null){
+            return false;
+        }
+        Matcher m = p.matcher(LastName);
+        return m.matches();
+
+    }
+
+    public static boolean emailValidation(String Email){
+        String regexEmail = "^[0-9 A-Z a-z]+(([._+-]*)[0-9A-Za-z]+)*@[0-9 A-Z a-z]+.[a-z]{2,4}([.][a-z]{2,3})*$";
+        Pattern p = Pattern.compile(regexEmail);
+        if (Email == null) {
+            return false;
+        }
+        Matcher m = p.matcher(Email);
+        return  m.matches();
 
     }
 
@@ -25,7 +47,12 @@ public class UserMain {
 
         System.out.println("Please Enter your lastname: ");
         String LastName = input.nextLine();
-        System.out.println(firstNameValidation(LastName));
+        System.out.println(lastNameValidation(LastName));
+
+        System.out.println("Please Enter your Email: ");
+        String EmailID = input.nextLine();
+        System.out.println(emailValidation(EmailID));
+
 
     }
 }
