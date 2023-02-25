64c02be157da60f659944631938b548066690b95
shilpa123 <shilpabiradar100@gmail.com>
2023-02-25 22:17:33 +0530

    ADDED: Validation For Last Name

--- src/main/java/org/example/UserMain.java
+++ src/main/java/org/example/UserMain.java
@@ -4,22 +4,28 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserMain {
-    public static boolean firstNameValidation(String FirstName){
+    public static boolean firstNameValidation(String FirstName) {
         String regex = "^[A-Z]{1}[a-z A-Z]{2,}$";
         Pattern p = Pattern.compile(regex);
-        if (FirstName == null){
+        if (FirstName == null) {
             return false;
         }
         Matcher m = p.matcher(FirstName);
         return m.matches();
 
     }
+
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Registration System");
         Scanner input = new Scanner(System.in);
 
-        System.out.print("Please Enter firstname: ");
+        System.out.print("Please Enter your firstname: ");
         String FirstName = input.nextLine();
         System.out.println(firstNameValidation(FirstName));
+
+        System.out.println("Please Enter your lastname: ");
+        String LastName = input.nextLine();
+        System.out.println(firstNameValidation(LastName));
+
     }
 }
